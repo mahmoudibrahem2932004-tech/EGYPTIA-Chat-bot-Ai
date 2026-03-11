f616ef6ee9af3bf65a506ed78cab45b6312fd531
mahmoudibrahem2932004-tech <mahmoudibrahem2932004@gmail.com>
2026-03-11 07:18:01 +0200

    Add HTML interface

--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 cd /app
-rasa train
+echo "تشغيل Rasa actions..."
 rasa run actions &
-rasa run --enable-api --cors "*" -p 7860
+echo "تشغيل Rasa server..."
+rasa run --enable-api --cors "*" -p 5005 &
+echo "تشغيل واجهة Gradio..."
+python app.py &
+echo "تشغيل خادم HTTP لملف HTML..."
+python -m http.server 7860 --bind 0.0.0.0
